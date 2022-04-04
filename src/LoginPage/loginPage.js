addb946d7071d9215c1d62d71681f5af55487c04
Britania Rodriguez Reyes <bxr180014@tamu.edu>
2022-04-04 14:39:31 -0500

    Create basic user Dashboard

--- src/LoginPage/loginPage.js
+++ src/LoginPage/loginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./loginPage.css";
+import {Link } from "react-router-dom";
 
 class LoginPage extends Component {
     state = {};
@@ -10,7 +11,7 @@ class LoginPage extends Component {
                 <input type="email"></input>
                 <p>Password</p>
                 <input type="password"></input>
-                <button className="signupbutton center">Login</button>
+                <Link to="/dashboard"><button className="signupbutton center">Login</button></Link>
             </div>
         );
     }
--- src/reactRouter.js
+++ src/reactRouter.js
@@ -4,6 +4,7 @@ import HeadingBar from "./HeadingBar/headingBar";
 import LandingPage from "./LandingPage/landingPage";
 import LoginPage from "./LoginPage/loginPage";
 import SignupPage from "./SignupPage/signupPage";
+import DashboardPage from "./DashboardPage/dashboardPage";
 
 export default function App() {
     return (
@@ -13,6 +14,7 @@ export default function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
             </Routes>
         </Router>
     );
