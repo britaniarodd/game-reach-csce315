63b806d42cd6885b73c972d5f2190a19402c9ad5
Jason Hirsch <jasonhi10@aol.com>
2022-04-03 21:37:49 -0500

    Started login page

--- src/LoginPage/loginPage.js
+++ src/LoginPage/loginPage.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import "./loginPage.css";
 
 class LoginPage extends Component {
     state = {};
     render() {
-        return <div className="background"></div>;
+        return (
+            <div className="background center">
+                <p className="emailtext">Email</p>
+                <input type="email"></input>
+                <p>Password</p>
+                <input type="password"></input>
+                <button className="signupbutton center">Login</button>
+            </div>
+        );
     }
 }
 
