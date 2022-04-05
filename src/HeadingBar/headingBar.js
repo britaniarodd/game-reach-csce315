71047b71c1fb2a2a8223ea7a3906c59911688041
Jason Hirsch <jasonhi10@aol.com>
2022-04-05 13:23:59 -0500

    Fixed heading bug

--- src/HeadingBar/headingBar.js
+++ src/HeadingBar/headingBar.js
@@ -9,7 +9,6 @@ class HeadingBar extends Component {
     render() {
         return (
             <div className="headingBar">
-<<<<<<< HEAD
                 <a className="cornera" href="/">
                     <img className="cornerlogo" src={cornerlogo}></img>
                 </a>
@@ -18,16 +17,6 @@ class HeadingBar extends Component {
                 </a>
                 <a className="profilea" href="/profile">
                     <img className="profile" src={profile}></img>
-=======
-                <a className="cornerlogo" href="/">
-                    <img src={cornerlogo} height="80px"></img>
-                </a>
-                <a className="headinglogo" href="/">
-                    <img src={headinglogo} height="80px"></img>
-                </a>
-                <a className="profile" href="/profile">
-                    <img src={profile} height="80px"></img>
->>>>>>> 3f68a6cd20cde4a25a18614d191ff48696287fa8
                 </a>
             </div>
         );
