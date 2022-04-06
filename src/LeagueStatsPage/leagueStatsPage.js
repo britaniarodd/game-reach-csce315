8d1cd734aa4c0502ca0d51ba52b6ed8e72468c34
Jason Hirsch <49957304+jason-hirsch@users.noreply.github.com>
2022-04-06 17:34:46 -0500

    Added rank detail and started match histroy

--- src/LeagueStatsPage/leagueStatsPage.js
+++ src/LeagueStatsPage/leagueStatsPage.js
@@ -17,12 +17,16 @@ class LeagueStatsPage extends Component {
         rank: emblem_bronze,
         wins: 0,
         losses: 0,
+        tier: "",
+        rankStr: "",
+        matches: [],
     };
 
     constructor(props) {
         super(props);
-        this.leagueAPIKey = "RGAPI-df3bde3a-3173-4242-8bd9-6d55944e0bec";
+        this.leagueAPIKey = "RGAPI-2253e6a5-31b7-4756-9c74-2cb377699762";
         this.apiRoute = "https://na1.api.riotgames.com";
+        this.americasApiRoute = "https://americas.api.riotgames.com";
     }
 
     render() {
@@ -38,6 +42,7 @@ class LeagueStatsPage extends Component {
                     <p>Account Level: {this.state.account_level}</p>
                     <p>Wins: {this.state.wins}</p>
                     <p>Losses: {this.state.losses}</p>
+                    <p>{this.state.tier} {this.state.rankStr}</p>
                     <img src={this.state.rank}></img>
                 </div>
             </div>
@@ -75,33 +80,43 @@ class LeagueStatsPage extends Component {
     setTier(tier) {
         switch (tier) {
             case "BRONZE":
-                this.setState({ rank: emblem_bronze });
+                this.setState({ rank: emblem_bronze, tier: "Bronze" });
                 break;
             case "CHALLENGER":
-                this.setState({ rank: emblem_challenger });
+                this.setState({ rank: emblem_challenger, tier: "Challenger" });
                 break;
             case "DIAMOND":
-                this.setState({ rank: emblem_diamond });
+                this.setState({ rank: emblem_diamond, tier: "Diamond" });
                 break;
             case "GOLD":
-                this.setState({ rank: emblem_gold });
+                this.setState({ rank: emblem_gold, tier: "Gold" });
                 break;
             case "GRANDMASTER":
-                this.setState({ rank: emblem_grandmaster });
+                this.setState({ rank: emblem_grandmaster, tier: "Grandmaster" });
                 break;
             case "IRON":
-                this.setState({ rank: emblem_iron });
+                this.setState({ rank: emblem_iron, tier: "Iron" });
                 break;
             case "MASTER":
-                this.setState({ rank: emblem_master });
+                this.setState({ rank: emblem_master, tier: "Master" });
                 break;
             case "PLATINUM":
-                this.setState({ rank: emblem_platinum });
+                this.setState({ rank: emblem_platinum, tier: "Platinum" });
                 break;
             case "SILVER":
-                this.setState({ rank: emblem_silver });
+                this.setState({ rank: emblem_silver, tier: "Silver" });
                 break;
         }
+    }
+
+    async getMatches(summoner_puuid) {
+        let request = this.americasApiRoute + "/lol/match/v5/matches/by-puuid/" + summoner_puuid + "/ids" + "?start=0&count=20&api_key=" + this.leagueAPIKey;
+        return Promise.resolve(await this.apiRequest(request));
+    }
+
+    async getMatchInfo(match_id) {
+        let request = this.americasApiRoute + "/lol/match/v5/matches/" + match_id + "?api_key=" + this.leagueAPIKey;
+        return Promise.resolve(await this.apiRequest(request));
     }
 
     async lookupUser(e) {
@@ -113,11 +128,19 @@ class LeagueStatsPage extends Component {
             });
 
             const encrypted_id = summonerJson.id;
+            const puuid = summonerJson.puuid;
 
             const rankJson = await this.getRankInfo(encrypted_id);
             const rankInfo = rankJson[0];
-            this.setState({ wins: rankInfo.wins, losses: rankInfo.losses });
+            this.setState({ wins: rankInfo.wins, losses: rankInfo.losses, rankStr: rankInfo.rank });
             this.setTier(rankInfo.tier);
+
+            const matchIdsJson = await this.getMatches(puuid);
+            for(let i = 0; i < 20; i++) {
+                const match_info = await this.getMatchInfo(matchIdsJson[i]);
+                console.log(match_info);
+                await new Promise(r => setTimeout(r, 100));
+            }
         }
     }
 }
