8b64742f0bb6c31cc30df9b7fe82e16fe4609fe9
Ryan Lin <54116754+ryanlin12@users.noreply.github.com>
2022-04-06 17:42:13 -0500

    Navbar Changes
    Changed font style of navbar

--- src/NavigationBar/navBar.js
+++ src/NavigationBar/navBar.js
@@ -9,26 +9,20 @@ import findConnections from "./../FindConnections/findConnectionsPage";
 class navBar extends Component {
     state = {  } ;
     render() { 
-        return (
-        /*<nav classNameName="navbar navbar-light bg-primary">
-            <a classNameName="navbar-brand" href="#">GR</a>
-        </nav>*/
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary ">
-        <a className="navbar-brand" href="#">GR</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="/dashboard">Dashboard <span className="sr-only">(current)</span></a>
-            <a className="nav-item nav-link active" href="/findConnections">Find Connections</a>
-            <a className="nav-item nav-link active" href="/viewConnections">View Connections</a>
+      return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand" href="#">GR</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-item nav-link active" href="/dashboard">Dashboard <span className="sr-only">(current)</span></a>
+                <a className="nav-item nav-link active" href="/findConnections">Find Connections</a>
+                <a className="nav-item nav-link active" href="/viewConnections">View Connections</a>
+            </div>
           </div>
-        </div>
-      </nav>
-
-
-
+        </nav>
         );
     }
 }
