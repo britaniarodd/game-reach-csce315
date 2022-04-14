af5a830156c9ea87538d8272a8f50327cf9af239
Jason Hirsch <49957304+jason-hirsch@users.noreply.github.com>
2022-04-14 14:54:21 -0500

    Working on backend for heroku

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.use('/users', usersRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
 });
 
 // error handler
