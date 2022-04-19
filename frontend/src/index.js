35e955e778b8bd523ddf185b23ba5c9bf080f331
Britania Rodriguez Reyes <bxr180014@tamu.edu>
2022-04-18 19:10:46 -0500

    fix login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./reactRouter";
 import "bootstrap/dist/css/bootstrap.css";
 
-window.name = "Britania Reyes";
+window.name = "";
 window.$email = "qhisklrj123@gmail.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
