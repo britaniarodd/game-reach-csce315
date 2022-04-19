354c2c138ca05a0521af8925f4b129d008adcdaa
Ryan Lin <54116754+ryanlin12@users.noreply.github.com>
2022-04-19 08:44:05 -0500

    FindConnections Search

--- frontend/src/FindConnections/findConnectionsPage.js
+++ frontend/src/FindConnections/findConnectionsPage.js
@@ -1,20 +1,45 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import "../shared.css";
 import "./findConnectionsPage.css";
+import Info from './../ViewConnections/info';
 import NavigationBar from "./../NavigationBar/navBar";
+import People from "./people.js";
+import {useState} from 'react'
+function FindConnections() {
+    const [searchTerm, setSearchTerm] = useState('')
+    return (
+        <React.Fragment>
+        <NavigationBar />
+        
+        <div className='background'>
+        <br/>
+        <h1 className = 'title'>Find Connections</h1>
 
-class findConnections extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <React.Fragment>
-            <NavigationBar />
-            <div className='background'>
-                <h1 className = 'title'>Find Connections</h1>
-            </div>
-            </React.Fragment>
-        );
-    }
+        <div className='App'>
+        <input type="text" placeholder='Search Players' onChange = {event => {setSearchTerm(event.target.value)}}/>
+        {People.filter((val => {
+            //filtering
+            if (searchTerm == ""){
+                return val;
+            }
+            else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+                return val;
+            }
+        })).map((val, key) =>{
+            return(
+                <div className = "user" key = {key}>
+                    <Info name={val.name} game={val.game} rank = {val.rank} status={val.status} bio={val.bio}/>
+                </div>
+            );
+        })}
+        </div>
+        </div>
+        
+        </React.Fragment>
+    );
+    
 }
- 
-export default findConnections;
+ //search bar: https://www.emgoto.com/react-search-bar/
+export default FindConnections;
+
+//push findConnectionsPage.js, findConnectionsPage.css, people.js
