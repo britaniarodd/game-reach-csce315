3b8a616ac018c7cc279eca3cc92aa3ae2843b82a
Jason Hirsch <jhirsch@tamu.edu>
2022-04-25 09:44:58 -0500

    Fixed db stuff

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 async function getUserByEmail(pgpool, user_email) {
     const poolres = await pgpool.query(
-        "SELECT user_id, email, status, bio, nickname FROM users WHERE email=$1",
+        "SELECT user_id, email, status, bio, nickname, discord FROM users WHERE email=$1",
         [user_email]
     );
     return poolres.rows[0];
@@ -8,7 +8,7 @@ async function getUserByEmail(pgpool, user_email) {
 
 async function getUserByUserId(pgpool, user_id) {
     const poolres = await pgpool.query(
-        "SELECT user_id, email, status, bio, nickname FROM users WHERE user_id=$1",
+        "SELECT user_id, email, status, bio, nickname, discord FROM users WHERE user_id=$1",
         [user_id]
     );
     return poolres.rows[0];
