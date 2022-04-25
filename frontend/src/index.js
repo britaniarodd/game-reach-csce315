d86e0ba9427039cca327d7feb92c7e30206b0259
Ryan Lin <54116754+ryanlin12@users.noreply.github.com>
2022-04-25 01:15:00 -0500

    Add files via upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./reactRouter";
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+
+window.name = "test2";
+window.email = "test2";
+window.loggedIn = false;
+window.leagueName = "";
+window.csgoName = "";
+window.apexName = "";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
