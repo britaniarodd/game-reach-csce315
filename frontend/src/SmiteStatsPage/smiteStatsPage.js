6618a63b55e4eeed21ebe0215403e5fb40226388
Jason Hirsch <jhirsch@tamu.edu>
2022-04-27 10:20:53 -0500

    stat

--- frontend/src/SmiteStatsPage/smiteStatsPage.js
+++ frontend/src/SmiteStatsPage/smiteStatsPage.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { getBackendAddress } from "../backendrequest";
+import { StatBox, StatElement } from "../StatsComponents";
 
 export default function SmiteStatsPage(props) {
+
+    const [statsJson, setStatsJson] = useState({});
+    const [errorMessage, setErrorMessage] = useState("");
 
     function lookupUser(e) {
         if (e.key === "Enter") {
             const ign = e.target.value;
-            
+            axios.get(getBackendAddress() + "/smiteapi/getstats/" + ign)
+            .then(res => {
+                setStatsJson(res.data);
+                setErrorMessage("");
+            })
+            .catch(err => {
+                if(err.response && err.response.status === 400) {
+                    setErrorMessage("Could not find player");
+                }
+            });
         }
     }
 
@@ -16,6 +30,13 @@ export default function SmiteStatsPage(props) {
                 <input type="text"
                     onKeyDown={lookupUser}
                 ></input>
+                <p>{errorMessage}</p>
+                <div className="d-flex">
+                    <StatBox>
+                        <StatElement title="Title1" value="value1" description="description1" />
+                        <StatElement title="Title2" value="value2" description="description2" />
+                    </StatBox>
+                </div>
         </div>
     );
 }
